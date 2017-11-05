796f90ced5788f7c4aad5a00ea9a265c88d0535a
vm109 <mangina.venkata@gmail.com>
2017-11-05 13:26:16 -0600

    Update SpringSecurityInterceptorApplication.java

--- src/main/java/org/my/spring/security/SpringSecurityInterceptorApplication.java
+++ src/main/java/org/my/spring/security/SpringSecurityInterceptorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSecurityInterceptorApplication {
-//changes needed in development [may need to change in hotfix]
+//changes needed in development [changes done on the hotfix branch]
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityInterceptorApplication.class, args);
 	}
