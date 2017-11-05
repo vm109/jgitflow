c0affc425e060555463d0dad74900dec1b5bfc19
venkat mangina <venkata.mangina@grainger.com>
2017-11-05 12:33:05 -0600

    commit to development branch

--- src/main/java/org/my/spring/security/SpringSecurityInterceptorApplication.java
+++ src/main/java/org/my/spring/security/SpringSecurityInterceptorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSecurityInterceptorApplication {
-//changes done for hotfix [these changes are done on hotfix with snapshot version and will merge to candidate]
+//changes needed in development [may need to change in hotfix]
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityInterceptorApplication.class, args);
 	}
